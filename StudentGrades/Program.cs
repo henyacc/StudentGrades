198ad7cd87941b825aa07a40622a7c81e352bdc6
Cecelia Coombs <chenyacc@yahoo.com>
2025-10-22 18:03:59 -0400

    completed making student class and created student objects, student objects to list, averaged grades and displayed to console

--- StudentGrades/Program.cs
+++ StudentGrades/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Student student = new Student();
+        List<Student> students = new List<Student>();
+        student.CalculateAverageGrade();
+
+        Student student1 = new Student()
+        {
+            StudentId = 1,
+            Grades = [90.1, 88.7, 100, 97.6],
+            Name = "Sharah",
+        };
+        
+        student1.AddGrade(90.8);
+        
+        Student student2 = new Student();
+        student2.StudentId = 2;
+        student2.Grades = [88.8, 90.6];
+        student2.Name = "Michael";
+        
+        student2.AddGrade(88.7);
+        
+        Student student3 = new Student();
+        student3.StudentId = 3;
+        student3.Grades = [98.2, 67.7, 55.5];
+        student3.Name = "David";
+        
+        student3.AddGrade(100);
+
+        Student student4 = new Student();
+        student4.StudentId = 4;
+        student4.Grades = [84.5, 85.9, 78.6];
+        student4.Name = "John";
+        
+        student4.AddGrade(85.9);
+        
+        students.AddRange([student1, student2, student3, student4]);
+
+        foreach (Student item in students)
+        {
+            Console.WriteLine($"Name: {item.Name}");
+            Console.WriteLine($"Student ID: {item.StudentId}");
+            Console.WriteLine("Grades:");
+
+            foreach (double grade in item.Grades)
+            {
+                Console.WriteLine(grade);
+            }
+            
+            
+            Console.WriteLine("Average grade:");
+            Console.WriteLine(Math.Round(item.CalculateAverageGrade(), 2));
+            Console.WriteLine();
+            
+            
+        }
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        
     }
 }
--- StudentGrades/Student.cs
+++ StudentGrades/Student.cs
@@ -2,5 +2,26 @@ namespace StudentGrades;
 
 public class Student
 {
-    
+    public string Name { get; set; } = "";
+    public int StudentId { get; set; } = 0;
+    public List<double> Grades { get; set; } = [];
+
+
+    public void AddGrade(double grade)
+    {
+        Grades.Add(grade);
+
+    }
+
+    public void AddGrade(params double[] grades)
+    {
+        Grades.AddRange(grades);
+    }
+
+    public double CalculateAverageGrade()
+    {
+        double sum = Grades.Any() ? Grades.Average() : 0;
+        return sum;
+    }
+
 }
